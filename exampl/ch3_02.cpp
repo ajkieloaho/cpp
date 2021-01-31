52b33ed60f538a956f75130e99fc52ddc26e0b91
Antti-Jussi Kieloaho <aj@kieloaho.com>
2021-01-31 17:57:29 +0200

    added new examples

--- exampl/ch3_02.cpp
+++ exampl/ch3_02.cpp
@@ -1,2 +1,30 @@
 // chapter 3 example 1
-// enumeration
+// Operations with enumerations
+
+#include <iostream>
+#include <iomanip>
+
+int main() {
+    enum class Day {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
+    Day yesterday {Day::Monday}, today {Day::Tuesday}, tomorrow {Day::Wednesday};
+    const Day poets_day{Day::Friday};
+
+    enum class Punctuation : char {Comma = ',', Exclamation = '!', Question = '?'};
+    Punctuation ch {Punctuation::Comma};
+    
+    std::cout << "yesterday's value is " << static_cast<int>(yesterday)
+    << static_cast<char>(ch) << " but poets_day's is " << static_cast<int>(poets_day)
+    << static_cast<char>(Punctuation::Exclamation) << std::endl;
+
+    today = Day::Thursday;      // Assign new ...
+    ch = Punctuation::Question; // ... enumerator values
+    tomorrow = poets_day;       // Copy enumerator value
+
+    std::cout << "Is today's value(" << static_cast<int>(today)
+    << ") the same as poets_day(" << static_cast<int>(poets_day)
+    << ')' << static_cast<char>(ch) << std::endl;
+
+    // ch = tomorow;
+    // tomorrow = Friday;
+    // today = 6;
+}
---
+++ exampl/ch3_03.cpp
@@ -0,0 +1,47 @@
+// chapter 3 example 3
+// Demostrating scope, lifetime, and global variables
+
+#include <iostream>
+long count1{999L};      // Global count1
+double count2{3.14};    // Global count2
+int count3;             // Global count3 - default initilization
+
+int main() {
+    int count1{10};     // Hides global count1
+    int count3{50};     // Hides global count3
+
+    std::cout << "Value of outer count1 = " << count1
+    << "\nValue of global count1 = " << ::count1 
+    << "\nValue of global count2 = " << count2
+    << std::endl;
+
+{ /* New block scope starts here... */
+    int count1{20};     // this is a bew variable that hides the outer count1
+    int count2{30};     // this hides global count2
+    
+    std::cout << "\nValue of inner count1 = " << count1
+    << "\nValue of global count1 = " << ::count1 
+    << "\nValue of inner count2 = " << count2
+    << "\nValue of global count2 = " << ::count2
+    << std::endl;
+
+    count1 = ::count1 + 3;      // This sets inner count1 to global count1 + 3
+    ++::count1;                 // This changes global count1
+    
+    std::cout << "\nValue of inner count1 = " << count1
+    << "\nValue of global count1 = " << ::count1 
+    << std::endl;
+    
+    count3 += count2;
+    int count4 {};
+} /* ... and end here. */
+
+// std::cout << count4 << std::endl;    // count4 does not exist in this scope!
+
+std::cout << "\nValue of outer count1 = " << count1 
+<< "\nValue of outer count3 =  " << count3
+<< "\nValue of global count3 = " << ::count3
+<< "\nValue of global count2 = " << count2
+<< std::endl;
+
+}
